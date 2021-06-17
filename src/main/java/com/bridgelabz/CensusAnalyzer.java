93a2873be19d5dadc359aa9d7c4eacfc698b179e
datta1808 <bagaledatta18@gmail.com>
2021-06-17 10:13:13 +0530

    Welcome to Indian States Census Analyzer

---
+++ src/main/java/com/bridgelabz/CensusAnalyzer.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class CensusAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian States Census Analyzer");
+    }
+}

e71054c382e97494b86803371ea0ae4ae502a7e0
datta1808 <bagaledatta18@gmail.com>
2021-06-17 11:14:17 +0530

    ADDED : Test Case to Ensure the Number of Records Matches

---
+++ src/main/java/com/bridgelabz/CensusAnalyserException.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+class CensusAnalyserException extends Exception {
+
+    enum ExceptionType {
+        UNABLE_TO_PARSE, CENSUS_FILE_PROBLEM, HEADERS_INVALID, NOT_A_CSV_TYPE
+    }
+
+    ExceptionType type;
+
+    public CensusAnalyserException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+
+    public CensusAnalyserException(String message, ExceptionType type, Throwable cause) {
+        super(message, cause);
+        this.type = type;
+    }
+}
--- src/main/java/com/bridgelabz/CensusAnalyzer.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz;
-
-public class CensusAnalyzer {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Indian States Census Analyzer");
-    }
-}
